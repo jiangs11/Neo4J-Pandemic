c963fa592801f46c7a4580c1cce7650badd8c557
Edward Callihan <47329280+ECallihan@users.noreply.github.com>
2020-12-05 18:49:03 -0500

    Update InfectionAlgo.java

--- src/test/java/neo4j/PeopleWebBuilderStuff/InfectionAlgo.java
+++ src/test/java/neo4j/PeopleWebBuilderStuff/InfectionAlgo.java
@@ -1,24 +1,39 @@
-package neo4j.PeopleWebBuilderStuff;
+package neo4j.infection;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
+
+import org.neo4j.driver.Session;
+
+import neo4j.PeopleWebBuilderStuff.JobType;
+import neo4j.PeopleWebBuilderStuff.MaskUsage;
+import neo4j.PeopleWebBuilderStuff.Masks;
+import neo4j.PeopleWebBuilderStuff.PeopleBuilder;
+import neo4j.PeopleWebBuilderStuff.Person;
+import neo4j.PeopleWebBuilderStuff.Relationships;
+import neo4j.PeopleWebBuilderStuff.SocialGuidelines;
+import neo4j.pandemic.NeoOperations;
+
 import java.util.Random;
 
 /**
  * Infection Algorithm
  * @author Edward Callihan
+ * @author Christina Bannon
  *
  */
-public class InfectionAlgo {
+public class Infection {
+	
 	
 	// For Checking Luck Against Risk Of Infection
 	private static Random luck = new Random();
 	
 	/**
-	 * These are all the values to weight the different factors of infection
+	 * Values to weigh the different relationship factors of infection
 	 */
-	
 	
 	// Relationship Strength Values
 	private static double relWeak = 0.2;
@@ -27,9 +42,9 @@ public class InfectionAlgo {
 	
 	// Mask Type Values
 	private static double mType3 = 0.05; // N-95
-	private static double mType2 = 0.2; // 2-Layer Cloth
-	private static double mType1 = 0.9; // Gaiter
-	private static double mType0 = 1.0; // None
+	private static double mType2 = 0.2;  // 2-Layer Cloth
+	private static double mType1 = 0.9;  // Gaiter
+	private static double mType0 = 1.0;  // None
 	
 	// Mask Usage Consistency Values
 	private static double useAlways = 0.05;
@@ -64,11 +79,37 @@ public class InfectionAlgo {
 	private static double hermitTrue = 0.3;
 	private static double hermitFalse = 1.0;
 	
+	/**
+	 * Values to weigh the different event factors of infection
+	 */
+	private static double eventOutdoors = 0.2;
+	private static double eventIndoors = 0.8; 
+	
+	private static double ventilationUpgraded = 0.2;
+	private static double ventilationNotUpgraded = 0.8;
+
+	private static double carnivalEvent = 0.5;
+	private static double sportsEvent = 0.5;
+	private static double fleaMarketEvent = 0.7;
+	private static double weddingEvent = 0.8;
+	private static double concertEvent = 0.8;
+	private static double politicalEvent = 1;
+	
+	private static double masksEnforced = 0.5;
+	private static double masksNotEnforced= 1;
+	
+	private static double socialDistancingEvent = 0.5;
+	private static double nonSocialDistancingEvent = 1;
+	
+	private static double tempChecks = 0.5; 
+	private static double noTempChecks = 1;  
+	
+	private static double handSanitizerAvailable = 0.2;
+	private static double handSanitizerNotAvailable = .8; 
 	
 	/**
 	 * Mappings of feature categories to their respective values.
-	 */
-	
+	 */	
 	private static HashMap<Masks, Double> maskFactor = new HashMap<Masks, Double>(){
 		{
 			put(Masks.none, mType0);
@@ -80,11 +121,11 @@ public class InfectionAlgo {
 	
 	private static HashMap<MaskUsage, Double> maskUseFactor = new HashMap<MaskUsage, Double>(){
 		{
-			put(MaskUsage.Always, useAlways);
-			put(MaskUsage.VeryOften, useOften);
-			put(MaskUsage.Someimtes, useSometimes);
-			put(MaskUsage.Rarely, useRarely);
-			put(MaskUsage.Never, useNever);
+			put(MaskUsage.always, useAlways);
+			put(MaskUsage.veryOften, useOften);
+			put(MaskUsage.sometimes, useSometimes);
+			put(MaskUsage.rarely, useRarely);
+			put(MaskUsage.never, useNever);
 		}
 	};
 	
@@ -97,7 +138,7 @@ public class InfectionAlgo {
 			put(JobType.warehouse, jobWareHouse);
 			put(JobType.noPublicInteraction, jobNoInteraction);
 			put(JobType.grocery, jobGrocery);
-			put(JobType.servers, jobRestaurant);
+			put(JobType.restaurant, jobRestaurant);
 			put(JobType.teachers, jobTeachers);
 			put(JobType.medicalWorkers, jobMedical);
 		}
@@ -115,15 +156,6 @@ public class InfectionAlgo {
 	 * This relationshipFactor may have to be used if the person.getRelationships() changes to return
 	 * the enum.
 	 */
-	
-//	private HashMap<Relationships, Double> relationshipFactor = new HashMap<Relationships, Double>(){
-//		{
-//			put(Relationships.Weak, relWeak);
-//			put(Relationships.Medium, relMedium);
-//			put(Relationships.Strong, relStrong);
-//		}
-//	};
-	
 	private static HashMap<String, Double> relationshipFactor = new HashMap<String, Double>(){
 		{
 			put("weak", relWeak);
@@ -133,7 +165,63 @@ public class InfectionAlgo {
 	};
 	
 	// Begin Methods here
+	/*
+	 * Factor Weights.
+	 * 
+	 * Each set of weights describes the contribution to infection probability
+	 * from the various factors of interaction between an infected and a 
+	 * healthy person.
+	 */
 	
+	// Infection Type Weights
+	private static double airborneWT = 0.75;
+	private static double handToHandWT = 0.25;
+	
+	// Airborne Infection Weights
+	private static double maskWT = 0.5;
+	private static double jobAirWT = 0.3;
+	private static double sDistAirWT = 0.2;
+	
+	// Hand to Hand Infection Weights
+	private static double hWashWT = 0.5;
+	private static double jobHigieneWT = 0.3;
+	private static double sDistHigieneWT = 0.2;
+	
+	// Individual Health Infection Weights
+	private static double healthyWT = 0.4;
+	private static double infectedWT = 0.6;
+	
+	// Begin Methods here
+	public static double personalRisk(Person person) {
+		
+		
+		// Contact contribution
+		double hWashSub = 1 - (person.isHandWashing() ? washTrue : washFalse);
+		double hWashVal = hWashWT * hWashSub;
+		double jobHigieneSub = 1 - (jobTypeFactor.get(person.getJobType()));
+		double jobHigieneVal = jobHigieneWT * jobHigieneSub;
+		double sDistHigieneSub = 1 - (socialFactor.get(person.getSocialGuidelines()));
+		double sDistHigieneVal = sDistHigieneWT  * sDistHigieneSub;
+		double handToHandSub = hWashVal + jobHigieneVal + sDistHigieneVal;
+		double handToHandVal = handToHandWT * handToHandSub;
+		
+		// Airborne Contribution
+		double maskSub = 1 - (maskFactor.get(person.getMask()) * maskUseFactor.get(person.getMaskUsage()));
+		double maskVal = maskWT * maskSub;
+		double jobAirSub = 1 - (jobTypeFactor.get(person.getJobType()));
+		double jobAirVal = jobAirWT * jobAirSub;
+		double sDistAirSub = 1 - (socialFactor.get(person.getSocialGuidelines()));
+		double sDistAirVal = sDistAirWT * sDistAirSub;
+		double airborneSub = maskVal + jobAirVal + sDistAirVal;
+		double airborneVal = airborneWT * airborneSub;
+		
+		// Total Contribution Weight by (Infected or Healthy)
+		double infWT = person.isInfected() ? infectedWT : healthyWT;
+		double personalSub = handToHandVal + airborneVal;
+		double personalVal = infWT * personalSub;
+		
+		return personalVal;
+	}
 	
 	/**
 	 * Calculates the risk a person contributes to spreading the virus.
@@ -148,9 +236,10 @@ public class InfectionAlgo {
 		risk *= socialFactor.get(person.getSocialGuidelines());
 		risk *= person.isHandWashing() ? washTrue : washFalse;
 		risk *= person.isHermit() ? hermitTrue : hermitFalse;
+		System.out.println("risk= " + risk);
 		return risk;
 	}
-	
+
 	/**
 	 * Calculates the risk of passing an infection from a person to their contact.
 	 * @param person : infected individual
@@ -158,9 +247,9 @@ public class InfectionAlgo {
 	 * @param relationship : relationship strength
 	 * @return risk value of spreading infection
 	 */
-	public static double calculateInteractionRisk(Person person, Person contact, String relationship) {
-		double behaviorMod1 = calculateBehaviorRisk(person);
-		double behaviorMod2 = calculateBehaviorRisk(contact);
+	public static double calculateInteractionRisk(Person infected, Person contact, String relationship) {
+		double behaviorMod1 = personalRisk(infected);
+		double behaviorMod2 = personalRisk(contact);
 		double relationshipMod = relationshipFactor.get(relationship);
 		return (behaviorMod1 + behaviorMod2) * relationshipMod;
 	}
@@ -195,6 +284,68 @@ public class InfectionAlgo {
 		}
 		return newInfections;
 	}
+	
+	/**
+	 * NeoOperations.getHealthyNeighborsOfInfectedNodes to pull all of the healthy Person nodes who have 
+	 * been exposed to an infected node from db. 
+	 * Iterates through map of exposed individuals and calculates risk factor for each
+	 * If an Person object's risk factor outweighs a random luck factor, the Person is infected. 
+	 * 
+	 * @param session       Session object connecting to db
+	 */
+	public static void infectThroughNetwork(Session session) {
+		HashMap map = NeoOperations.getHealthyNeighborsOfInfectedNodes(session);
+		
+		HashMap infectedPeople = (HashMap)map.get("INFECTED");
+		HashMap exposedPeople = (HashMap)map.get("EXPOSED"); 
+		
+		Iterator exposedPeopleIterator = exposedPeople.entrySet().iterator(); 
+		while (exposedPeopleIterator.hasNext()) {
+            Map.Entry exposedPersonEntry= (Map.Entry)exposedPeopleIterator.next(); 
+            String exposedPersonId = exposedPersonEntry.getKey().toString(); 
+            
+            HashMap exposedPersonFieldMap = (HashMap)exposedPersonEntry.getValue();
+            
+            Person exposedPerson = (Person)exposedPersonFieldMap.get("person"); 
+            String infectedPersonId = (String)exposedPersonFieldMap.get("transmitterId");
+            String relationshipStrength = (String)exposedPersonFieldMap.get("relationship");
+            
+            String exposedPersonName = exposedPerson.getName();
+            Person infectedPerson = (Person)infectedPeople.get(infectedPersonId);
+            String infectedPersonName = infectedPerson.getName(); 
+            
+            double riskFactor = calculateInteractionRisk(exposedPerson, infectedPerson, relationshipStrength); 
+            double luckFactor = luck.nextDouble(); 
+            if (riskFactor > luckFactor) {
+            	System.out.println("infected person [" + infectedPersonName + 
+            			"] HAS infected [" + exposedPersonName + "] as their risk factor was [" + riskFactor +
+            			"] and their luck factor was [" + luckFactor + "] ");
+            	
+            } else {
+            	System.out.println("      infected person [" + infectedPersonName + 
+            			"] HAS NOT infected [" + exposedPersonName + "] as their risk factor was [" + riskFactor +
+            			"] and their luck factor was [" + luckFactor + "] ");
+            }
+		}
+	}
+	
+	private static Relationships getRelationshipsEnum(String relationshipStrength) {
+		Relationships strength = null; 
+		switch(relationshipStrength) {
+		case "strong":
+			strength = Relationships.Strong; 
+			break; 
+		case "medium":
+			strength = Relationships.Medium; 
+			break; 
+		case "weak":
+			strength = Relationships.Weak; 
+			break; 
+		}
+		return strength; 
+	}
+	
+	
 }
 
 	
