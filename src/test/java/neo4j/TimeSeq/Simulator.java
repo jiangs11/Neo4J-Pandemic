74260b23c416572fa7b01ca2f6362e21c7820947
Joe Leclercq <leclercqj8@students.rowan.edu>
2020-12-15 03:50:21 -0500

    Added the beginnings of the charts. Fixed formatting and methods related to them -JL

---
+++ src/test/java/neo4j/TimeSeq/Simulator.java
@@ -0,0 +1,18 @@
+package neo4j.TimeSeq;
+
+import neo4j.Driver
+
+public class Simulator {
+	/**
+	 * Static method that handles the running of the different
+	 * methods created by other groups. Will also call the 
+	 * graphing methods.
+	 * @param pids The id's for the nodes we created
+	 */
+	public static void simulate(ArrayList<Integer> pids) {
+		
+		
+	}
+	
+	
+}
