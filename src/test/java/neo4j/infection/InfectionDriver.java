5f89b38a3f42bb9e138f796e8794fc5904d9fb49
Joe Leclercq <leclercqj8@students.rowan.edu>
2020-12-17 19:21:18 -0500

    New changes

--- src/test/java/neo4j/infection/InfectionDriver.java
+++ src/test/java/neo4j/infection/InfectionDriver.java
@@ -17,36 +17,36 @@ import neo4j.pandemic.NeoOperations;
  */
 public class InfectionDriver {
 
-	public static void main(String [] args) {
-		NeoConnector nc = null; 
-		Session ses = null; 
-		try {
-			String bolt1 = "bolt://54.237.9.240:7687";  // Main DB Server
-			String bolt2 = "bolt://54.90.41.128:7687";  // Dedicated neo server
-			String bolt3 = "bolt://localhost:7687"; 	// Localhost
-
-			// Uncomment the bolt assignment that you want to use
-			//String bolt = bolt1;
-			String bolt = bolt3;
-
-			nc = new NeoConnector(bolt, "neo4j", "Christina");
-			ses = nc.getDriver().session();
-			
-			/**
-			 * 61549214400000
-			 */
-			
-			createEvents(ses);
-			Long ms = new Long("61558977600000");
-//			Infection.infectThruNetwork(ses); 		
-			Infection.infectThruEvent(ses, new Date(ms));
-			
-			ses.close();
-			nc.close();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-	}
+//	public static void main(String [] args) {
+//		NeoConnector nc = null; 
+//		Session ses = null; 
+//		try {
+//			String bolt1 = "bolt://54.237.9.240:7687";  // Main DB Server
+//			String bolt2 = "bolt://54.90.41.128:7687";  // Dedicated neo server
+//			String bolt3 = "bolt://localhost:7687"; 	// Localhost
+//
+//			// Uncomment the bolt assignment that you want to use
+//			//String bolt = bolt1;
+//			String bolt = bolt3;
+//
+//			nc = new NeoConnector(bolt, "neo4j", "Christina");
+//			ses = nc.getDriver().session();
+//			
+//			/**
+//			 * 61549214400000
+//			 */
+//			
+//			createEvents(ses);
+//			Long ms = new Long("61558977600000");
+////			Infection.infectThruNetwork(ses); 		
+//			Infection.infectThruEvent(ses, new Date(ms));
+//			
+//			ses.close();
+//			nc.close();
+//		} catch (Exception e) {
+//			System.out.println(e.getMessage());
+//		}
+//	}
 	
 	public static void createEvents(Session ses) {
 		EventBuilder.fillEventHolder(50);
